6dfc0ba246f2f877dade8c97799e2ef21397a4df
Pier77Repeter <pier77repeter@gmail.com>
2024-08-03 18:43:29 +0200

    Added new banner

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ navigator.clipboard.writeText('DiversityCraft.my.pebble.host')
 });
 
 const randomImage = document.getElementById('randomImage');
-const imageArray = ['clicktocopyiplogo.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg'];
+const imageArray = ['clicktocopyiplogo.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg', 'banner-6.jpg'];
 
 function getRandomImage() {
     const randomIndex = Math.floor(Math.random() * imageArray.length);
