cf07c2b4acd4a76f0381ea8f36c7aac61d4158a5
Pier77Repeter <pier77repeter@gmail.com>
2024-12-25 10:43:22 +0100

    Copyright updated, some text updates and new banner

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ navigator.clipboard.writeText('diversitycraft.xyz')
 });
 
 const randomImage = document.getElementById('randomImage');
-const imageArray = ['clicktocopyiplogo.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg', 'banner-6.jpg', 'banner-7.jpg', 'banner-8.jpg'];
+const imageArray = ['banner.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg', 'banner-6.jpg', 'banner-7.jpg', 'banner-8.jpg', 'banner-9.jpg'];
 
 function getRandomImage() {
     const randomIndex = Math.floor(Math.random() * imageArray.length);
