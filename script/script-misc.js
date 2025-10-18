bfd40d1c937cb0502cbdb61ed18f03452540a668
Pier77Repeter <pier77repeter@gmail.com>
2025-10-18 14:51:51 +0200

    Format changes, improved test page, updated links and other changes

--- script/script-misc.js
+++ script/script-misc.js
@@ -1,9 +1,20 @@
-const randomImage = document.getElementById('randomImage');
-const imageArray = ['banner.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg', 'banner-6.jpg', 'banner-7.jpg', 'banner-8.jpg', 'banner-9.jpg'];
+const randomImage = document.getElementById("randomImage");
+const imageArray = [
+  "banner.jpg",
+  "banner-1.jpg",
+  "banner-2.jpg",
+  "banner-3.jpg",
+  "banner-4.jpg",
+  "banner-5.jpg",
+  "banner-6.jpg",
+  "banner-7.jpg",
+  "banner-8.jpg",
+  "banner-9.jpg",
+];
 
 function getRandomImage() {
-    const randomIndex = Math.floor(Math.random() * imageArray.length);
-    randomImage.src = '../media/' + imageArray[randomIndex];
+  const randomIndex = Math.floor(Math.random() * imageArray.length);
+  randomImage.src = "../media/" + imageArray[randomIndex];
 }
 
 getRandomImage();
--- script/script.js
+++ script/script.js
@@ -1,27 +1,39 @@
-const copyButton = document.getElementById('copy-ip');
-const copyMessage = document.getElementById('copy-message');
+const copyButton = document.getElementById("copy-ip");
+const copyMessage = document.getElementById("copy-message");
 
-copyButton.addEventListener('click', () => {
-navigator.clipboard.writeText('diversitycraft.xyz')
+copyButton.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText("diversitycraft.xyz")
     .then(() => {
-        copyMessage.textContent = 'Server IP copied, time to join in!';
-        copyMessage.style.color = 'green';
-        setTimeout(() => {
-        copyMessage.textContent = '';
-        }, 3000);
+      copyMessage.textContent = "Server IP copied, time to join in!";
+      copyMessage.style.color = "green";
+      setTimeout(() => {
+        copyMessage.textContent = "";
+      }, 3000);
     })
-    .catch(err => {
-        copyMessage.textContent = 'Copy failed, somewhow...';
-        copyMessage.style.color = 'red';
+    .catch((err) => {
+      copyMessage.textContent = "Copy failed, somewhow...";
+      copyMessage.style.color = "red";
     });
 });
 
-const randomImage = document.getElementById('randomImage');
-const imageArray = ['banner.jpg', 'banner-1.jpg', 'banner-2.jpg', 'banner-3.jpg', 'banner-4.jpg', 'banner-5.jpg', 'banner-6.jpg', 'banner-7.jpg', 'banner-8.jpg', 'banner-9.jpg'];
+const randomImage = document.getElementById("randomImage");
+const imageArray = [
+  "banner.jpg",
+  "banner-1.jpg",
+  "banner-2.jpg",
+  "banner-3.jpg",
+  "banner-4.jpg",
+  "banner-5.jpg",
+  "banner-6.jpg",
+  "banner-7.jpg",
+  "banner-8.jpg",
+  "banner-9.jpg",
+];
 
 function getRandomImage() {
-    const randomIndex = Math.floor(Math.random() * imageArray.length);
-    randomImage.src = '../media/' + imageArray[randomIndex];
+  const randomIndex = Math.floor(Math.random() * imageArray.length);
+  randomImage.src = "../media/" + imageArray[randomIndex];
 }
 
 getRandomImage();
