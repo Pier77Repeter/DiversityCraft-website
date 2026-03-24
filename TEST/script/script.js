25871b77563ad1b828dcda3ddd95db762ae824d2
Pier77Repeter <pier77repeter@gmail.com>
2026-03-24 11:33:15 +0100

    Added TEST dir for website redesign

---
+++ TEST/script/script.js
@@ -0,0 +1,43 @@
+const copyButton = document.getElementById("copy-ip");
+const copyMessage = document.getElementById("copy-message");
+
+copyButton.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText("diversitycraft.org")
+    .then(() => {
+      copyMessage.textContent = "Server IP copied, time to join in!";
+      copyMessage.style.color = "green";
+      copyMessage.style.fontFamily = "Inter, Helvetica";
+      copyMessage.style.fontSize = "20px";
+      setTimeout(() => {
+        copyMessage.textContent = "";
+      }, 3000);
+    })
+    .catch((err) => {
+      copyMessage.textContent = "Copy failed, somewhow...";
+      copyMessage.style.color = "red";
+      copyMessage.style.fontFamily = "Inter, Helvetica";
+      copyMessage.style.fontSize = "20px";
+    });
+});
+
+const randomImage = document.getElementById("randomImage");
+const imageArray = [
+  "banner.jpg",
+  "banner-1.jpg",
+  "banner-2.jpg",
+  "banner-3.jpg",
+  "banner-4.jpg",
+  "banner-5.jpg",
+  "banner-6.jpg",
+  "banner-7.jpg",
+  "banner-8.jpg",
+  "banner-9.jpg",
+];
+
+function getRandomImage() {
+  const randomIndex = Math.floor(Math.random() * imageArray.length);
+  randomImage.src = "../media/" + imageArray[randomIndex];
+}
+
+getRandomImage();
